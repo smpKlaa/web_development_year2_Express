9d210392f7ab76ed03699de7409401b552cbea89
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2025-11-11 19:04:36 +0200

    fix upload.js file naming

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -7,13 +7,15 @@ const createThumbnail = async (req, res, next) => {
     next();
     return;
   }
-
   sharp(req.file.path)
     .resize(160, 160)
     .toFormat('jpeg')
     .toBuffer()
     .then((data) => {
-      const name = path.basename(req.file.filename, '.jpg');
+      const name = path.basename(
+        req.file.filename,
+        path.extname(req.file.filename)
+      );
       fs.writeFile(`uploads/${name}_thumb.jpeg`, data, (err) => {
         if (err) {
           console.log(err);
