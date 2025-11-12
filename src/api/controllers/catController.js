274162c96e870661d62ba666e379ac218f1ec47a
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2025-11-12 19:14:22 +0200

    convert to using only filename in cats "filename" column

--- src/api/controllers/catController.js
+++ src/api/controllers/catController.js
@@ -6,17 +6,20 @@ import {
   removeCat,
   findCatsByOwnerId,
 } from '../models/catModel.js';
+import 'dotenv/config.js';
+import process from 'process';
 
 const getCats = async (req, res) => {
   console.log('GET all cats.');
-  res.json(await listAllCats());
+  const cats = await listAllCats();
+  res.json(cats.map(serializeCat));
 };
 
 const getCatById = async (req, res) => {
   console.log('GET cat by id.');
   const cat = await findCatById(req.params.id);
   if (cat) {
-    res.json(cat);
+    res.json(serializeCat(cat));
   } else {
     res.sendStatus(404);
   }
@@ -26,7 +29,7 @@ const getCatByOwnerId = async (req, res) => {
   console.log('GET cat by owner id.');
   const cat = await findCatsByOwnerId(req.params.ownerId);
   if (cat) {
-    res.json(cat);
+    res.json(serializeCat(cat));
   } else {
     res.sendStatus(404);
   }
@@ -38,13 +41,14 @@ const postCat = async (req, res) => {
   console.log('Request form data: ', req.body);
   console.log('Request image metadata: ', req.file);
 
-  const filePath = `http://${req.get('host')}/uploads/${req.file.filename}`;
+  // const filepath = `http://${req.get('host')}/uploads/${req.file.filename}`;
+
   const {cat_name, weight, owner, birthdate} = req.body;
   const result = await addCat({
     cat_name: cat_name,
     weight: weight,
     owner: owner,
-    filename: filePath,
+    filename: req.file.filename,
     birthdate: birthdate,
   });
 
@@ -58,10 +62,10 @@ const postCat = async (req, res) => {
 
 const putCat = async (req, res) => {
   console.log('PUT cat.');
-  if (req.file) {
-    const filePath = `http://${req.get('host')}/uploads/${req.file.filename}`;
-    req.body.filename = filePath;
-  }
+  // if (req.file) {
+  //   const filePath = `http://${req.get('host')}/uploads/${req.file.filename}`;
+  //   req.body.filename = filePath;
+  // }
   const result = await replaceCat(req.params.id, req.body);
   if (result) {
     res.status(200);
@@ -83,5 +87,22 @@ const deleteCat = async (req, res) => {
     res.sendStatus(404);
   }
 };
+
+function getCatImageUrl(filename) {
+  if (!filename) return null;
+
+  const url = `http://${process.env.DOMAIN}/uploads/${filename}`;
+
+  return url;
+}
+
+const serializeCat = (row) => ({
+  cat_id: row.cat_id,
+  cat_name: row.cat_name,
+  weight: row.weight,
+  owner: row.owner,
+  filename: getCatImageUrl(row.filename),
+  birthdate: row.birthdate,
+});
 
 export {getCats, getCatById, getCatByOwnerId, postCat, putCat, deleteCat};
