0da695c3b639bd08985a799f0f1dc28e8ff1eea9
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2025-11-12 20:24:35 +0200

    add authorization exception for admin role users

--- src/middlewares/authorizeUser.js
+++ src/middlewares/authorizeUser.js
@@ -1,5 +1,16 @@
+// import {findUserById} from '../api/models/userModel.js';
+
 const authorizeUser = async (req, res, next) => {
   console.log('authorizeUser', res.locals.user);
+
+  // Automatically authorize admin users.
+  // Querying database may be more secure.
+  //   const user = await findUserById(res.locals.user.user_id);
+  if (res.locals.user.role === 'admin') {
+    console.log('Admin authorized');
+    next();
+    return;
+  }
 
   if (req.params.id == res.locals.user.user_id) {
     console.log('Request authorized');
--- src/middlewares/autorizeCatOwner.js
+++ src/middlewares/autorizeCatOwner.js
@@ -1,7 +1,17 @@
 import {findCatById} from '../api/models/catModel.js';
+// import {findUserById} from '../api/models/userModel.js';
 
 const authorizeCatOwner = async (req, res, next) => {
   console.log('authorizeCatOwner', res.locals.user);
+
+  // Automatically authorize admin users.
+  // Querying database may be more secure.
+  // const user = await findUserById(res.locals.user.user_id);
+  if (res.locals.user.role === 'admin') {
+    console.log('Admin authorized');
+    next();
+    return;
+  }
 
   if (req.method === 'POST') {
     if (req.body.owner == res.locals.user.user_id) {
